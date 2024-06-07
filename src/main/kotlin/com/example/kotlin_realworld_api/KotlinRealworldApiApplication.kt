5608ba4554ddf85fe0a7c3aeb5d5d713413b3429
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-06-07 23:12:19 +0900

    first commit

---
+++ src/main/kotlin/com/example/kotlin_realworld_api/KotlinRealworldApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_realworld_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRealworldApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinRealworldApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlin_realworld_api/KotlinRealworldApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin_realworld_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRealworldApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
