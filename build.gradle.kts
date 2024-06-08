3f94e8006fc0f88277634fb0b7174bd1997b5833
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-06-08 18:49:22 +0900

    feat: openapi.generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+
 plugins {
     kotlin("jvm") version "2.0.0"
     kotlin("plugin.spring") version "2.0.0"
     id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
+    id("org.openapi.generator") version "7.6.0"
 }
 
 group = "com.example"
@@ -19,16 +22,57 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.springdoc:springdoc-openapi-ui:1.8.0")
+    implementation("io.swagger.core.v3:swagger-annotations:2.2.22")
+    implementation("org.springframework.boot:spring-boot-starter-validation:3.3.0")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.register<GenerateTask>("buildMarkdownApiDoc") {
+    generatorName.set("markdown")
+    inputSpec.set("$rootDir/src/main/resources/openapi.yaml")
+    outputDir.set("$buildDir/api-doc")
+    doLast {
+        val readmeFile = file("$rootDir/README.md")
+        val generatedDoc = file("$buildDir/api-doc/README.md")
+        if (generatedDoc.exists()) {
+            readmeFile.writeText(generatedDoc.readText())
+        } else {
+            throw GradleException("Generated Markdown API doc not found: $generatedDoc")
+        }
+    }
+}
+
+openApiGenerate {
+    generatorName.set("kotlin-spring")
+    inputSpec.set("$rootDir/src/main/resources/openapi.yaml")
+    outputDir.set("$buildDir/spring")
+    apiPackage.set("com.example.todoapi.controller")
+    modelPackage.set("com.example.todoapi.model")
+    configOptions.set(
+        mapOf(
+            "interfaceOnly" to "true",
+            "useSpringBoot3" to "true"
+        )
+    )
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs("$buildDir/spring/src/main/kotlin")
+        }
+    }
 }
 
 kotlin {
